921b2065dcc2e517b11bbb9060d779995e70cb40
Jayanth Mayur H <jayanthmayurh@gmail.com>
2025-08-19 10:12:17 +0530

    Abstract class 1

---
+++ AbstractionClass.java
@@ -0,0 +1,37 @@
+/*
+ * The Abstract Class
+ * You cannot create an object of this class.
+ * It acts as a blueprint for other classes.
+ */
+abstract class Animals1{
+    // An abstract method. It has no body.
+    // This forces subclasses to provide an implementation.
+    public abstract void Animalsound();
+
+    //regular method
+    public void sleep(){
+        System.out.println("zzz");
+    }
+
+}
+//child class
+/*
+ * The Subclass
+ * It MUST implement all abstract methods from the parent.
+ */
+class Dog9 extends Animals1{
+    public void Animalsound(){
+         // Here, we provide the body for the abstract animalSound() method.
+        System.out.println("Bow Bow");
+    }
+}
+
+//Main  class
+public class AbstractionClass{
+    public static void main(String[] args) {
+        Dog9 myDog2 = new Dog9();
+        myDog2.Animalsound();
+        myDog2.sleep();
+    }
+}
+
